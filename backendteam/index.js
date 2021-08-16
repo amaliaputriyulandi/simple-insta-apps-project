f6beb78b0579cb6a543d543ae2632291e71fc704
darin <darinadia98@gmail.com>
2021-08-16 23:25:45 +0700

    - Init Project Backend

---
+++ backendteam/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+
+const server = express();
+const port = process.env.PORT || 3000;
+// require("./database/config")();
+
+server.use(logger("dev"));
+server.use(cors());
+server.use(express.json());
+server.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+server.all("*", (req, res) => {
+  res.status(404).json({
+    statusText: "Not Found",
+    message: "You Have Trying Reaching A Route That Doesn't Exist",
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
